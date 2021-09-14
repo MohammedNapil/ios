b17ef340a417798704f1ebb1cafe655f42c86968
mohammed napil <90660013+MohammedNapil@users.noreply.github.com>
2021-09-14 03:38:45 +0200

    add oop

---
+++ ta3lem.playground/Contents.swift
@@ -0,0 +1,31 @@
+protocol tapolvew {
+    func namperorraw() ->Int
+    func showtaeem(intex : Int)
+}
+class folwrs : tapolvew {
+     
+    var arreuoser = ["moahmmed","napil","antr"]
+    
+    
+    func namperorraw() -> Int {
+        return arreuoser.count
+    }
+    
+    func showtaeem(intex: Int) {
+        print (" intex\(intex) , item = \(arreuoser[intex])")
+    }
+    
+}
+var user1 = folwrs()
+user1.arreuoser
+user1.namperorraw()
+user1.self
+user1.showtaeem(intex: 2)
+
+
+
+
+
+
+
+
